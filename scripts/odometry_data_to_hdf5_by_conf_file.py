9af502a5e4604cb97f201de8bf659e1145f2b40b
Velas Martin <ivelas@merlin.fit.vutbr.cz>
2017-03-28 14:01:07 +0200

    Fix: building hdf5 by conf file

--- scripts/odometry_data_to_hdf5_by_conf_file.py
+++ scripts/odometry_data_to_hdf5_by_conf_file.py
@@ -6,7 +6,7 @@ import math
 import random
 import h5py
 
-if len(sys.argv) != 1:
+if len(sys.argv) != 2:
     sys.stderr.write("ERROR, arguments: [output-file.hdf5]\n")
     sys.exit(1)
 
@@ -15,15 +15,17 @@ data = []
 for l in lines:
     tokens = l.strip().split()
     odometry = map(float, tokens[0:6])
-    angle_classes = map(int, tokens[6:9])
+    angle_classes = map(float, tokens[6:9])
     files = tokens[9:]
     data.append({"odometry":odometry, "rot_class_x":angle_classes[0], 
-                 "rot_class_y":angle_classes[1], "rot_class_z":angle_classes[1],
+                 "rot_class_y":angle_classes[1], "rot_class_z":angle_classes[2],
                  "data":files})
 
 dato = np.load(data[0]["data"][0])
 rings, width, channels = dato.shape
 frames_per_odometry = len(data[0]["data"])
+
+print (len(data), channels*frames_per_odometry, rings, width)
 
 with h5py.File(sys.argv[1], 'w') as output_file:
     output_file.create_dataset('data', (len(data), channels*frames_per_odometry, rings, width), dtype='f4')
@@ -33,10 +35,12 @@ with h5py.File(sys.argv[1], 'w') as output_file:
     output_file.create_dataset('rot_class_z', (len(data),), dtype='f4')
 
     for di, dato in enumerate(data):
-        for fi, file in dato["data"]:
+        for fi, file in enumerate(dato["data"]):
             frame = np.load(file)
+            print frame.shape
             frame = np.moveaxis(frame, [0, 1, 2], [1, 2, 0])
-            output_file["data"][di, fi*channels, ...] = frame
+            print frame.shape
+            output_file["data"][di, fi*channels:(fi+1)*channels, ...] = frame
         output_file["odometry"][di, :] = np.array(dato["odometry"])
         output_file["rot_class_x"][di] = dato["rot_class_x"]
         output_file["rot_class_y"][di] = dato["rot_class_y"]
