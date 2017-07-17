52e71cd16cda56cbb120a660d1f85ff36ec8f7e0
Martin Velas <mato.velas@gmail.com>
2017-07-17 14:51:12 +0200

    No classes in conf HDF5 files

--- scripts/odometry_data_to_hdf5_by_conf_file.py
+++ scripts/odometry_data_to_hdf5_by_conf_file.py
@@ -15,11 +15,8 @@ data = []
 for l in lines:
     tokens = l.strip().split()
     odometry = map(float, tokens[0:6])
-    angle_classes = map(float, tokens[6:9])
-    files = tokens[9:]
-    data.append({"odometry":odometry, "rot_class_x":angle_classes[0], 
-                 "rot_class_y":angle_classes[1], "rot_class_z":angle_classes[2],
-                 "data":files})
+    files = tokens[6:]
+    data.append({"odometry":odometry, "data":files})
 
 dato = np.load(data[0]["data"][0])
 rings, width, channels = dato.shape
@@ -28,9 +25,6 @@ frames_per_odometry = len(data[0]["data"])
 with h5py.File(sys.argv[1], 'w') as output_file:
     output_file.create_dataset('data', (len(data), channels*frames_per_odometry, rings, width), dtype='f4')
     output_file.create_dataset('odometry',    (len(data), 6), dtype='f4')
-    output_file.create_dataset('rot_class_x', (len(data),), dtype='f4')
-    output_file.create_dataset('rot_class_y', (len(data),), dtype='f4')
-    output_file.create_dataset('rot_class_z', (len(data),), dtype='f4')
 
     for di, dato in enumerate(data):
         for fi, file in enumerate(dato["data"]):
@@ -38,6 +32,3 @@ with h5py.File(sys.argv[1], 'w') as output_file:
             frame = np.moveaxis(frame, [0, 1, 2], [1, 2, 0])
             output_file["data"][di, fi*channels:(fi+1)*channels, ...] = frame
         output_file["odometry"][di, :] = np.array(dato["odometry"])
-        output_file["rot_class_x"][di] = dato["rot_class_x"]
-        output_file["rot_class_y"][di] = dato["rot_class_y"]
-        output_file["rot_class_z"][di] = dato["rot_class_z"]
