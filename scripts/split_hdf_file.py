c34b9d97620f1996a5eeec3ef6fdc219400a0aa8
Velas Martin <ivelas@merlin.fit.vutbr.cz>
2017-07-24 14:42:34 +0200

    Split HDF into fixed sized chunks

--- scripts/split_hdf_file.py
+++ scripts/split_hdf_file.py
@@ -5,9 +5,7 @@ import numpy as np
 import h5py
 import os
 
-BATCH_SIZE=16
-MAX_SIZE = 1800*1000*1000 # 2GB
-OUT_LAYERS = ['rot_class_x', 'rot_class_y', 'rot_class_z']
+FILE_SIZE=96
 
 filenames = []
 fi = 1
@@ -24,24 +22,18 @@ if len(out_filename) == 0 or len(filenames) == 0:
     sys.exit(1)
 
 for filename in filenames:
-    split_times = os.stat(filename).st_size/(MAX_SIZE)
     with h5py.File(filename, "r") as hf:
         data = hf["data"]
         odometry = hf["odometry"]
         odom_count = np.shape(odometry)[0]
         hsize = np.shape(data)[0]/odom_count
+        parts = odom_count/FILE_SIZE + (1 if odom_count%FILE_SIZE > 0 else 0)
 
-        rot_classes = {}
-        if OUT_LAYERS[0] in hf: 
-            for rot_layer in OUT_LAYERS:
-                rot_classes[rot_layer] = hf[rot_layer]
+        print filename, "will be split into", parts, "parts"
 
-        print filename, "will be split", split_times-1, "times"
-
-        max_new_odoms_count = odom_count / (split_times+1) + odom_count % (split_times+1)
         odoms_copied = 0
-        for s in range(split_times):
-            new_odoms_count = min(max_new_odoms_count, odom_count-odoms_copied)
+        for s in range(parts):
+            new_odoms_count = min(FILE_SIZE, odom_count-odoms_copied)
             new_filename = out_filename + ".%s"%(s)
             print new_filename
             output_file = h5py.File(new_filename, 'w')
@@ -49,9 +41,5 @@ for filename in filenames:
             output_file.create_dataset('odometry', (new_odoms_count,) + np.shape(odometry)[1:], dtype='f4')
             output_file['odometry'][...] = odometry[odoms_copied:odoms_copied+new_odoms_count]
             output_file['data'][...] = data[odoms_copied*hsize:(odoms_copied+new_odoms_count)*hsize]
-            if len(rot_classes) > 0:
-                for rot_layer in OUT_LAYERS:
-                    output_file.create_dataset(rot_layer, (new_odoms_count,), dtype='f4')
-                    output_file[rot_layer][...] = rot_classes[rot_layer][odoms_copied:odoms_copied+new_odoms_count]
 
             odoms_copied += new_odoms_count
